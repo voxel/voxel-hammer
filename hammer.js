28a1b74cd90838b3c12aa8a5a6a28d253b3cd47a
deathcap <deathcap@gmx.co.uk>
2014-06-03 21:16:30 -0700

    Carve out a 3x3x1 area perpendicular to the normal

---
+++ hammer.js
@@ -0,0 +1,70 @@
+'use strict';
+
+module.exports = function(game, opts) {
+  return new HammerPlugin(game, opts);
+};
+module.exports.pluginInfo = {
+  loadAfter: ['voxel-registry', 'voxel-mine', 'voxel-inventory-hotbar', 'voxel-recipes']
+};
+
+function HammerPlugin(game, opts) {
+  this.game = game;
+
+  this.registry = game.plugins.get('voxel-registry');
+  if (!this.registry) throw new Error('voxel-hammer requires voxel-registry plugin');
+
+  this.mine = game.plugins.get('voxel-mine');
+  if (!this.mine) throw new Error('voxel-hammer requires voxel-mine');
+
+  this.hotbar = game.plugins.get('voxel-inventory-hotbar'); // TODO: carry?
+  if (!this.hotbar) throw new Error('voxel-hammer requires voxel-inventory-hotbar');
+
+  this.recipes = game.plugins.get('voxel-recipes'); // optional
+
+  this.enable();
+};
+
+HammerPlugin.prototype.enable = function() {
+  this.registry.registerItem('hammerIron', {
+    itemTexture: 'items/iron_pickaxe', // TODO
+    displayName: 'Iron Hammer',
+    toolClass: 'pickaxe',
+  });
+  if (this.recipes) {
+    this.recipes.registerPositional([
+        ['ingotIron', 'ingotIron', 'ingotIron'],
+        ['ingotIron', 'stick', 'ingotIron'],
+        [undefined, 'stick', undefined]], ['hammerIron']);
+  }
+  this.mine.on('break', this.break.bind(this));
+};
+
+HammerPlugin.prototype.disable = function() {
+};
+
+var around = function(cb) {
+  [-1, 0, 1].forEach(function(dx) {
+    [-1, 0, 1].forEach(function(dy) {
+      [-1, 0, 1].forEach(function(dz) {
+        cb(dx, dy, dz);
+      });
+    });
+  });
+};
+
+HammerPlugin.prototype.break = function(target) {
+  var heldItem = this.hotbar.held();
+
+  if (!heldItem || heldItem.item !== 'hammerIron') return;
+
+  console.log(target);
+  around(function(dx, dy, dz) {
+    var x = target.voxel[0] + (target.normal[0] === 0 ? dx : 0);
+    var y = target.voxel[1] + (target.normal[1] === 0 ? dy : 0);
+    var z = target.voxel[2] + (target.normal[2] === 0 ? dz : 0);
+    console.log(x,y,z);
+
+    this.game.setBlock([x,y,z],0);
+    //this.mine.emit('break', {voxel:[x,y,z]}); // TODO: how to report a third-party block break?
+  }.bind(this));
+};
